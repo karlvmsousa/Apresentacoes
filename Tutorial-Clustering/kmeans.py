4443d55b30c472a6bae7def9ac99b3d5e59b7f0a
karlvandesman <karlvandesman@gmail.com>
2019-10-23 14:16:48 -0300

    correções kmeans.py

--- Tutorial-Clustering/kmeans.py
+++ Tutorial-Clustering/kmeans.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 19 2019
-
+Tutorial Kmeans
 @author: karlvandesman
 """
-# *** Importação de bibliotecas ***
+
+# *** Importar bibliotecas ***
 from sklearn.datasets import make_blobs
 import numpy as np
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 
 #%%
-# Algoritmo K-means
-
-# *** Inicialização de Centroids ***
+# *** Inicializar centroides ***
 def kMeansInicCentroides(X, K):
-	# Seleciona os centros dos clusters iniciais
+    '''Seleciona aleatoriamente clusters dentre os exemplos de X'''
 	
     centroidesIniciais = X[np.random.choice(X.shape[0], K, replace=False), :]
     
@@ -24,96 +22,72 @@ def kMeansInicCentroides(X, K):
 
 #%%
 # *** Encontrar centroide mais próximo ***
-
 def encontraCentroidesProximos(X, centroides):
-    ''' Calcula o centróide mais próximo de cada exemplo'''
-    # Inputs:
+    '''A partir da distância euclidiana, calcula-se o centroide mais próximo 
+    para cada exemplo. Com isso, atribui-se a esse exemplo um grupo.
     # Retorna:
     # Vetor de comprimento n_exemplos com valores dos índices dos centroides
-    # n_exemplos = X.shape[0]
-
+    '''
+    
     indicesCentroidesProximos = np.zeros(len(X))
     
-    for i in range(len(X)):
-        #distancia = [ distance.euclidean(X_exemplo, i) for i in centroides]
-        
-        distancia = [ distance.euclidean(X[i], j) for j in centroides]
+    for i in range(len(X)):     
+        distancia = [ distance.euclidean(X[i], j) for j in centroides ]
         cluster = np.argmin(distancia)
         indicesCentroidesProximos[i] = cluster
             
     return indicesCentroidesProximos
 
 #%%
-# *** Cálculo de médias ***
-def calcularMediasCentroides(X, idc, K):
-    ''' Calcula a média dos valores dos exemplos de cada centroide '''
-        
+# *** Calcular valor médio para os centroides ***
+def calcularMediaCentroides(X, indicesGrupos, K):
+    '''Dado os exemplos atribuídos a cada grupo, calcula-se a média
+    para '''
+    centroides = []
+    
     for i in range(K):
-    #pontos = 
-        centroides[i] = np.mean(X[idc==i][:], axis=0)
-        #print("média com índice = 0", np.mean(X[idx==0][:]))
-    #print(centroides)
-        
+        centroides.append(np.mean(X[indicesGrupos==i][:], axis=0))
+
     return np.vstack(centroides)
 
 #%%
-# *** Algoritmo K-médias ***
-
-def kMeans(X, K, num_iter, plot_caminho=False):
-    # Inicializa aleatoriamente os centroides
+# *** Algoritmo K-means ***
+def kMeans(X, K, num_iter, detalhado=False):
+    '''A partir de um conjunto de dados e um número de grupos previamente
+    definido, encontra-se o melhor agrupamento dos dados'''
+    
     plt.figure()
 
     centroides = kMeansInicCentroides(X, K)
     plt.scatter(centroides[:, 0], centroides[:, 1], marker='X', 
-                        alpha=0.8, c='m', s=150)
+                        alpha=0.4, c='m', s=150)
     
     # Loop de iteração para aproximação dos grupos aos centroides
     for _ in range(num_iter):
-        # A cada iteração, atribui-se inicialmente cada exemplo ao centroide
-        # mais próximo
+        # A cada iteração, atribui-se cada exemplo ao centroide mais próximo
         indicesGrupos = encontraCentroidesProximos(X, centroides)
         
-        # Atualiza o valor dos centroide para a média dos exemplos atribuidos
-        # a ele.
-        #centroides = novosCentroides
+        # Atualiza o valor dos centroides, e guarda-se os centroides antigos
         centroidesAntigos = centroides
-        centroides = calcularMediasCentroides(X, indicesGrupos, K)
+        centroides = calcularMediaCentroides(X, indicesGrupos, K)
         
-        print("São iguais?", np.array_equal(centroidesAntigos, centroides))
-        print("Centroides antigos: ")
-        print(centroidesAntigos)
-        print("Centroides atualizados: ")
-        print(centroides)
+        # Plotar caminho de atualização de centroides se especificado
+        if detalhado: plotDetalhado(centroides, centroidesAntigos)
         
-        if plot_caminho:
-            # Plotar os centroides e o caminho feito após as atualizações
-            for j in range(K):
-                plt.arrow(centroidesAntigos[j, 0], centroidesAntigos[j, 1], 
-                          centroides[j, 0] - centroidesAntigos[j, 0], 
-                          centroides[j, 1] - centroidesAntigos[j, 1])
-#            for j in range(K):
-#                plt.plot(centroidesAntigos[j, 0], centroides[j, 1], "g--",
-#                         alpha=0.7)
-        
-        plt.scatter(centroides[:, 0], centroides[:, 1], marker='*', c='k', 
-                    s=200, alpha = 0.6)
-        #plotKmeans(X, centroides, indicesGrupos)
-        
-        # Condição de parada: se os centroides antigos e novos são iguais, 
-        # então não está havendo modificação na atualização.
-        if np.array_equal(centroidesAntigos, centroides):
-            break
+        # O algoritmo para se não há diferença na atualização dos centroides
+        if np.array_equal(centroidesAntigos, centroides): break
     
     plotKmeans(X, centroides, indicesGrupos)
-    #plt.show()
+    
     return centroides, indicesGrupos
 
 #%%
-# *** Plot atualização centroides ***
-
+# *** Plot dados agrupados por clusters ***
 def plotKmeans(X, centroides, indicesGrupos):
-
-    LABEL_COLOR_MAP = {0 : 'r', 1 : 'b'}
+    '''Plota um gráfico dos dados agrupados pelos centroides, com cor
+    específica para cada grupo'''
+    
+    LABEL_COLOR_MAP = {0 : 'r', 1 : 'b', 2: 'y', 3:'g'}
     label_color = [LABEL_COLOR_MAP[l] for l in indicesGrupos]
     
     plt.scatter(X[:, 0], X[:, 1], s=6, c=label_color)
@@ -122,22 +96,24 @@ def plotKmeans(X, centroides, indicesGrupos):
     plt.show()
 
 #%%
-# *** Aplicação ***
-
-#X = np.random.randint(50, size=(50, 2))
+# *** Plot da diferença na atualização dos centroides ***
+def plotDetalhado(centroides, centroidesAntigos):
+    '''Plotar os centroides e o caminho feito após as atualizações'''
     
+    K = centroides.shape[0]
+    
+    for j in range(K):
+        plt.arrow(centroidesAntigos[j, 0], centroidesAntigos[j, 1], 
+                  centroides[j, 0] - centroidesAntigos[j, 0], 
+                  centroides[j, 1] - centroidesAntigos[j, 1])
+    
+    plt.scatter(centroides[:, 0], centroides[:, 1], marker='*', c='k', 
+                s=200, alpha = 0.6)    
+
+#%%
+# *** Exemplo de aplicação ***
+
 # Geração de base de dados para agrupamento
 X, y = make_blobs(n_samples=100, random_state=19, centers=3, cluster_std=2)
 
-# 1) Gera centroides aleatoriamente
-#centroides = kMeansInicCentroides(X, 2)
-
-# 2) A partir dos centroides, calcula o centroide mais próximo de cada exempl.
-#idc = encontraCentroidesProximos(X, centroides)
-
-# Uso do algoritmo geral que retorna o centroide final, e o o cluster a qual
-# pertence cada exemplo
-
-centroides, indicesGrupos = kMeans(X, 3, 5, plot_caminho=True)
-
-#plotKmeans(X, centroides, indicesGrupos)
+centroides, indicesGrupos = kMeans(X, K=3, num_iter=5, detalhado=True)
